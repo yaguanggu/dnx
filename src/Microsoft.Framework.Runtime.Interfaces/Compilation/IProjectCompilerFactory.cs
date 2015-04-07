f47f176c251fdd71b792afda9244cfc0edf4f755
anurse <andrew@andrewnurse.net>
2015-04-07 09:57:46 -0700

    stash ProjectCompilerFactory work

---
+++ src/Microsoft.Framework.Runtime.Interfaces/Compilation/IProjectCompilerFactory.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using Microsoft.Framework.Runtime.Caching;
+
+namespace Microsoft.Framework.Runtime.Compilation
+{
+    public interface IProjectCompilerFactory
+    {
+        IProjectCompiler CreateCompiler(
+            ICache cache,
+            ICacheContextAccessor cacheContextAccessor,
+            INamedCacheDependencyProvider namedCacheDependencyProvider,
+            IFileWatcher fileWatcher,
+            IApplicationEnvironment applicationEnvironment,
+            IAssemblyLoadContextFactory loadContextFactory);
+    }
+}
